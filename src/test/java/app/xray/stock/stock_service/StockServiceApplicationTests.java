0d71f3166fcd9ab2f73cb522cf28eb36474905b2
slicequeue <slicequeue@gmail.com>
2025-06-10 21:23:45 +0900

    chore: 프로젝트 최초 커밋

---
+++ src/test/java/app/xray/stock/stock_service/StockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package app.xray.stock.stock_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
